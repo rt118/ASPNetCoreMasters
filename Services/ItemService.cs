7e05e192b0c93ba64bb543d6374760984ebc05cf
rt118 <omed.billy@gmail.com>
2022-08-08 14:46:30 +0800

    Test

--- Services/ItemService.cs
+++ Services/ItemService.cs
@@ -3,7 +3,6 @@
     public class ItemService : IItemService
     {
         public IEnumerable<string> GetAll() {
-
           return new List<string>() {  
             "String 1",
             "String 2",
